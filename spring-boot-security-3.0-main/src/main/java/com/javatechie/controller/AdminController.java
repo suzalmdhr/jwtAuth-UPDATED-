8d87c9b2057f1e3ea040a8aa9470f0642f14fa90
Suzal <sujalmdhr10@gmail.com>
2024-09-13 11:21:24 +0545

    First commit

---
+++ spring-boot-security-3.0-main/src/main/java/com/javatechie/controller/AdminController.java
@@ -0,0 +1,16 @@
+package com.javatechie.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping
+    public String adminAccess(){
+        return "Only admin can access this";
+    }
+
+}
---
+++ spring-boot-security-3.0-main/src/main/java/com/javatechie/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.javatechie.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+
+    @GetMapping
+    public String checkUser(){
+        return "User can only access this one";
+    }
+
+}
